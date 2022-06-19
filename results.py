03782f0cb55c5fcd550f98d61da348f1c4b89186
Jakub Królik <01142179@pw.edu.pl>
2022-06-19 21:44:08 +0200

    Add results data

--- results.py
+++ results.py
@@ -2,5 +2,5 @@ import cocopp
 import os
 import webbrowser
 
-cocopp.main('cma-fmin2-001.tgz scipy-optimize-fmin.tgz')
+cocopp.main('cma.tgz external-cma.tgz ego.tgz parEgo.tgz')
 webbrowser.open("file://" + os.getcwd() + "/ppdata/index.html")
